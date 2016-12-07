51c391522977f34d36b52d1ec9239891ce9f2dac
Tom Coleman <tom@thesnail.org>
2016-12-07 12:01:11 +1100

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function findByIds(collection, ids) {
+  return collection.find({ _id: { $in: ids } })
+    .toArray()
+    .then((docs) => {
+      const idMap = {};
+      docs.forEach((d) => { idMap[d._id] = d; });
+      return ids.map(id => idMap[id]);
+    });
+}
+
